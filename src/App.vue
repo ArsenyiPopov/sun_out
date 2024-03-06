be68cb44ab837f363882cc85d3b89171a0ffa244
Arsnyi <arsenyi.popov@gmail.com>
2024-03-06 13:37:37 +0300

    bg-sun

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+h1 {
+  margin: 0;
+  padding: 0;
+  color: white;
+}
+p {
+  color: white;
 }
 </style>
