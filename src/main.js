c7f70322d3ae94d2949180ec8f35cd0b3dcef504
Arsnyi <arsenyi.popov@gmail.com>
2024-03-09 17:54:57 +0300

    ya metrica

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
-import { createHead } from '@unhead/vue';
+import { createHead } from '@unhead/vue'; 
 import App from './App.vue';
 import router from './router';
-
+import VueYandexMetrika from 'vue-yandex-metrika'
 
 const app = createApp(App);
 
@@ -11,6 +11,12 @@ const head = createHead();
 app.use(head);
 
 app.use(router);
+// eslint-disable-next-line 
+Vue.use(VueYandexMetrika, {
+    id: 96694010,
+    env: process.env.NODE_ENV
+    
+}); 
 
 app.mount('#app');
 
