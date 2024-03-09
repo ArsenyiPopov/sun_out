f826b34920ebddbad53d9456ce9b1853622027af
Arsnyi <arsenyi.popov@gmail.com>
2024-03-09 17:39:55 +0300

    googl

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue';
-import VueYandexMetrika from 'vue-v3-yandex-metrika';
 import App from './App.vue';
 import router from './router';
 
@@ -9,20 +8,9 @@ const app = createApp(App);
 
 const head = createHead();
 
-app.use(head)
+app.use(head);
 
 app.use(router);
-
-app.use(VueYandexMetrika, {
-	id: 96694010,
-    trackLinks:true,
-    accurateTrackBounce:true,
-    webvisor:true,
-	router: router,
-	env: process.env.NODE_ENV
-    // other options
-})
-
 
 app.mount('#app');
 
