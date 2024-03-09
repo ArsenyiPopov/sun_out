aff7c5ce9bb2e68269124adb2511fd2cfde4957a
Arsnyi <arsenyi.popov@gmail.com>
2024-03-09 17:31:38 +0300

    googl and metrica ya

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //main.js
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue';
+import VueYandexMetrika from 'vue-v3-yandex-metrika';
 import App from './App.vue';
 import router from './router';
 
@@ -13,6 +14,15 @@ app.use(head)
 
 app.use(router);
 
+app.use(VueYandexMetrika, {
+	id: 96694010,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true,
+	router: router,
+	env: process.env.NODE_ENV
+    // other options
+})
 
 
 app.mount('#app');
